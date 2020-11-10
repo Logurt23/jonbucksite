fb28c92ed931925c16dfbb2af24744f3d490a0e6
Logan Brewer <logan@fbstudios.com>
2020-11-10 16:59:00 -0600

    JB Push

--- base/views.py
+++ base/views.py
@@ -10,3 +10,12 @@ def homepage(request):
     return render(request, 'base/homepage.html', context={
 
     })
+
+def project1(request):
+    return render(request, 'base/project1.html',)
+
+def project2(request):
+    return render(request, 'base/project2.html',)
+
+def project3(request):
+    return render(request, 'base/project3.html',)
