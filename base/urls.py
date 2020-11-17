a2c881aa148c97eb0a0a479a1d06b046c5e12bf2
William Knox <william@fbstudios.com>
2020-11-17 14:40:23 -0600

    all links now work

--- base/urls.py
+++ base/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('about.html/', about, name='about'),
     path('contact/', contact, name='contact'),
     path('projectlist/', projectlist, name='projectlist'),
-    path('project1/', project1, name='project1'),
-    path('project2/', project2, name='project2'),
-    path('project3/', project3, name='project3'),
+    path('project1.html/', project1, name='project1'),
+    path('project2.html/', project2, name='project2'),
+    path('project3.html/', project3, name='project3'),
 ]
